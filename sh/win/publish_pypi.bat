7b42a26bd275caedf517197c7963594aecf57734
Philip Loguinov <philip.loguinov@onetick.com>
2017-09-30 19:58:38 -0400

    fix license

--- sh/win/publish_pypi.bat
+++ sh/win/publish_pypi.bat
@@ -7,7 +7,7 @@ set PIP_CONFIG_FILE=C:\dev\secrets\pip.ini
 @pushd %PROJECT_PATH%
 
 rem -- ToDo: macro to obtain __version__
-twine upload %PROJECT_PATH%\dist\powertools-0.0.1-py3-none-any.whl --comment "factoring utilities from smash"
+twine upload %PROJECT_PATH%\dist\powertools-0.0.2-py3-none-any.whl --comment "fixing pypi description"
 
 @popd
 @endlocal
